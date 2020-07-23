9f107a518c2d2f71eed1c458e4868ed83b289272
Patrick <patrick910826@gmail.com>
2020-07-23 11:44:17 +0800

    MCU code optomixe on 7/23

--- MCU_code/V2/CTRL/Controller.ino
+++ MCU_code/V2/CTRL/Controller.ino
@@ -1,11 +1,14 @@
 void CTRL(){
   // get value from controller [controller value]
     int valueFromController=analogRead(A1);                   //value could be from 0~1023, normal value should be within 180~880
-    CTRLP = map(valueFromController,182,880,0,100);   //turn analogvalue to servo degree
+    CTRLP = map(valueFromController,177,880,0,100);   //turn analogvalue to servo degree
+    if (CTRLP<0) CTRLP =0;
     if (CTRLP != LCTRLP){
       Serial4.print("X");
       Serial4.print(CTRLP);
-      CTRLP = LCTRLP;
+      Serial.print("X");
+      Serial.print(CTRLP);
+      LCTRLP = CTRLP;
     }
     
   //Take botton be press on the controller
